3abf41bda3a7a6e27b3670e2079c8c24aa4cbd6d
djjohnson565 <derekjjohnson565@gmail.com>
2024-12-12 03:10:39 -0500

    see changelog

--- Radio/Radio.ino
+++ Radio/Radio.ino
@@ -1,83 +1,95 @@
 /*
 Pocket Radio Project
 */
-//INSTALL THE RADIO LIBRARY BY MARTHERTEL https://github.com/mathertel/Radio/archive/master.zip
 #include <Arduino.h>
-#include <TEA5767.h>
 #include <Wire.h>
-#include <SSD1306Wire.h>
 #include <radio.h>
-#include <RDSParser.h>
+#include <TEA5767.h>
 #include <SI4703.h>
+#include <SSD1306Wire.h>
 
-#define RESET_PIN 4
-#define MODE_PIN 21
+//Function Buttons
+const byte buttons[] = {34,0,35};
+const int UP = buttons[0];
+const int TOG = buttons[1];
+const int DOWN = buttons[2];
 
 #define FREQ A10 //Potentiometer Input
-const byte buttons[] = {34,0,35};
-#define BACK buttons[0];
-#define HOME buttons[1];
-#define NEXT buttons[2];
 #define MIN 88.0 //Min Frequency
 #define MAX 108.0 //Max Frequency
+
+//Screen Dimensions
 #define WIDTH 128
 #define HEIGHT 64
-#define MAX_FRAME_DELAY 128
-#define TOUCH_THRESHOLD 25
 
+//Initialize Radios
 TEA5767 radio = TEA5767();
+SI4703 radioInfo = SI4703();
 
-SI4703 radioInfo; //Si4703 Model for Radio information
-RDSParser rds; //RDS handler
-
+//Initialize LCD
 SSD1306Wire lcd(0x3c, SDA, SCL);
-int freq_read; //Potentiometer Input
-float frequency = 100.7; //Radio Station
-int SAMPLES = 10;
 
-/*
-void DisplayServiceName(const char *name) {
-  Serial.print("RDS:");
-  Serial.println(name);
-  // lcd.print(name);
-}  // DisplayServiceName()
+int freq_read; //potentiometer reading
+float frequency = 88.0;
+bool up_pressed;
+bool toggle_pressed;
+bool down_pressed;
+bool last_toggle_state = HIGH;
+bool curr_toggle_state = HIGH;
+bool state;
 
+//Non-blocking delay (main loop)
+unsigned long previousMillis = 0;
+const long interval = 500;
+unsigned long currentMillis = millis();
 
-/// Update the RDS Text
-void DisplayRDSText(const char *txt) {
-  Serial.print("Text:");
-  Serial.println(txt);
-  lcd.drawString(10, 20, "Text: ");
-  lcd.drawString(50, 20, txt);
-}  // DisplayRDSText()
+//Non-blocking delay (buttons)
+unsigned long previousMillis_button = 0;
+const long interval_button = 100;
+unsigned long currentMillis_button = millis();
 
+struct RadioStation {
+  String frequency;
+  String callSign;
+  String format;
+};
 
-void DisplayTime(uint8_t hour, uint8_t minute) {
-  Serial.print("RDS-Time:");
-  if (hour < 10) Serial.print('0');
-  Serial.print(hour);
-  Serial.print(':');
-  if (minute < 10) Serial.print('0');
-  Serial.println(minute);
-}  // DisplayTime()
-*/
+//local Amherst, MA stations: https://radio-locator.com/cgi-bin/locate?select=city&city=Amherst&state=MA
+RadioStation stations[] = {
+  {"88.5", "WFCR", "Public"},
+  {"89.3", "WAMH", "College"},
+  {"90.3", "WAMC", "Public"},
+  {"91.1", "WMUA", "UMass"},
+  {"91.9", "WOZQ", "College"},
+  {"92.3", "W222CH", "Oldies"},
+  {"93.1", "WHYN", "Cont."},
+  {"93.9", "WRSI", "Album"},
+  {"94.3", "W232BW", "Hits"},
+  {"94.7", "WMAS", "Holiday"},
+  {"95.3", "WPVQ", "Country"},
+  {"96.1", "WSRS", "Holiday"},
+  {"96.5", "WTIC", "Cont."},
+  {"96.9", "W245BK", "Oldies"},
+  {"97.3", "WKXE", "???"},
+  {"97.7", "W249DP", "Hits"},
+  {"98.3", "WHAI", "Cont."},
+  {"98.9", "W255DL", "News"},
+  {"99.3", "WLZX", "Rock"},
+  {"99.9", "WKMY", "Christian"},
+  {"100.7", "WZLX", "Rock"},
+  {"100.9", "WRNX", "Country"},
+  {"101.1", "WGIR", "Rock"},
+  {"101.5", "W268CZ", "News"},
+  {"102.1", "WAQY", "Rock"},
+  {"103.3", "WXOJ", "Variety"},
+  {"104.9", "WYRY", "Country"},
+  {"105.5", "WWEI", "Sports"},
+  {"106.3", "WEIB", "Jazz"},
+  {"106.9", "WCCC", "Christian"},
+  {"107.7", "WAIY", "Religious"}
+};
 
-/// Display a label and value for several purpose
-// void DisplayMenuValue(String label, int value) {
-//   String out = label + ": " + value;
-
-//   Serial.println(out);
-
-//   lcd.setCursor(0, 1);
-//   lcd.print(out + "  ");
-// }
-
-void RDS_process(uint16_t block1, uint16_t block2, uint16_t block3, uint16_t block4) {
-  rds.processData(block1, block2, block3, block4);
-}
-
-//draw the main menu screen
-//Very rough idea of what it looks like for now, "Back", "Home", "Next" refer to 3 buttons on Makerboard B0, B1, B3
+//This needs a new layout once the RDS info format has been found
 void draw_main() {
   lcd.clear();
   lcd.drawRect(0, 0, WIDTH, HEIGHT);
@@ -88,97 +100,94 @@ void draw_main() {
   lcd.drawString(10, 30, "Artist: ");
   lcd.drawString(50, 30, "Sample Artist");
   lcd.drawRect(10, HEIGHT - 20, 30, 15);
-  lcd.drawString(12, HEIGHT - 20, "Back");
+  lcd.drawString(12, HEIGHT - 20, "Down");
   lcd.drawRect(52, HEIGHT - 20, 30, 15);
-  lcd.drawString(54, HEIGHT - 20, "Home");
+  lcd.drawString(54, HEIGHT - 20, "Tog");
   lcd.drawRect(95, HEIGHT - 20, 30, 15);
-  lcd.drawString(97, HEIGHT - 20, "Next");
+  lcd.drawString(97, HEIGHT - 20, "Up");
+  if(!state) {
+    lcd.drawString(10, 1, "Knob");
+  }else {
+    lcd.drawString(10, 1, "Buttons");
+  }
   lcd.display();
 }
 
-//There is not Song, Artist sections because I don't know the format of the txt string extracted from RDS. After testing and seeing I can format it again.
-//The thing drew inplace of those sections is the full raw text extract from RDS (see the displayRDSText), i think
-void draw_real() {
-  lcd.clear();
-  lcd.drawRect(0, 0, WIDTH, HEIGHT);
-  lcd.drawString(10, 10, "Station: ");
-  lcd.drawString(50, 10, String(frequency) + "FM");
-  lcd.drawRect(10, HEIGHT - 20, 30, 15);
-  lcd.drawString(12, HEIGHT - 20, "Back");
-  lcd.drawRect(52, HEIGHT - 20, 30, 15);
-  lcd.drawString(54, HEIGHT - 20, "Home");
-  lcd.drawRect(95, HEIGHT - 20, 30, 15);
-  lcd.drawString(97, HEIGHT - 20, "Next");
-  lcd.display();
+float get_freq_pot() {
+  freq_read = 0;
+  for(int i = 0; i < 10; i++) {
+    freq_read += analogRead(FREQ);
+  }
+  freq_read /= 10;
+  return map(freq_read, 0, 4095, MIN * 10, MAX * 10) / 10.0;
+}
+
+void get_button_read() {
+  if(up_pressed) {
+      if(frequency >= 108.0) {
+        frequency = 88.0;
+      }else {
+        frequency += 0.1;
+      }
+      delay(200);
+    }else if(down_pressed) {
+      if(frequency <= 88.0) {
+        frequency = 108.0;
+      }else {
+        frequency -= 0.1;
+      }
+      delay(200);
+    }
 }
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
+  //radioInfo.init();
 
   radio.setFrequency(frequency);
-  /*
-  radioInfo.setBandFrequency(RADIO_BAND_FM, int(frequency*100));
-  radioInfo.setMute(true);
-  
-  //Setup for the Si4703
-  radioInfo.setup(RADIO_RESETPIN, RESET_PIN);
-  radioInfo.setup(RADIO_MODEPIN, MODE_PIN);
-
-  // Enable information to the Serial port
-  radioInfo.debugEnable(true);
-  radioInfo._wireDebug(true);
-
-  radioInfo.setup(RADIO_FMSPACING, RADIO_FMSPACING_200);   // for US
-  radioInfo.setup(RADIO_DEEMPHASIS, RADIO_DEEMPHASIS_75);  // for US
-
-  radioInfo.initWire(Wire);
-  */
-//Adding a function to proccess the RDS data to the radio module when it received RDS data
-  //radioInfo.attachReceiveRDS(RDS_process);
-//Adding functions to do when the RDS data is processed 
-  //rds.attachServiceNameCallback(DisplayServiceName); //Service Provider
-  //rds.attachTextCallback(DisplayRDSText);  //The full RDS data (don't know the format yet)
-//Tell how long we are playing the radio?
-  //rds.attachTimeCallback(DisplayTime);
+  //radioInfo.setBandFrequency(RADIO_BAND_FM, int(frequency*10));
+  //radioInfo.setMute(true);
 
   for(int i = 0;i < 3;i++) {
-    pinMode(buttons[i], INPUT_PULLUP);
+    pinMode(buttons[i], INPUT);
   }
+
   lcd.init();
   lcd.flipScreenVertically();
   lcd.setFont(ArialMT_Plain_10);
-  // lcd.drawString(64, 10, String(frequency));
-  // lcd.clear();
-  // lcd.display();
+  lcd.clear();
+  lcd.display();
   draw_main();
 }
 void loop() {
-  char rdsData[10];
-  //radioInfo.formatFrequency(rdsData, sizeof(rdsData)); //format the frequency for printing, ALTERNATIVE OPTION
-  freq_read = 0;
-  for(int i = 0; i < SAMPLES; i++) {
-    freq_read += analogRead(FREQ);
-  }
-  freq_read /= SAMPLES;
-  frequency = map(freq_read, 0, 4095, MIN * 10, MAX * 10) / 10.0;
-  radio.setFrequency(frequency);   //Uncommented because the main circuit is not connected,just testing the lcd displau for now
-  //radioInfo.setFrequency(map(freq_read, 0, 4095, MIN * 100, MAX * 100)); //Set the frequency to be the same as the TEA5767, different format
-  Serial.print(frequency);
-  Serial.print(", Unfiltered: ");
-  Serial.println(analogRead(FREQ));
-
-  //radioInfo.checkRDS(); //Check if the RDS data is good
-  RADIO_INFO info;
-  //radioInfo.getRadioInfo(&info);
-  draw_main();
-  /*
-  if (!info.rds) {
+  currentMillis = millis();
+  if(currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    up_pressed = (digitalRead(UP) == LOW);
+    toggle_pressed = (digitalRead(TOG) == LOW);
+    down_pressed = (digitalRead(DOWN) == LOW);
+    currentMillis_button = millis();
+    curr_toggle_state = digitalRead(TOG);
+    if(last_toggle_state == HIGH && curr_toggle_state == LOW) {
+      state = !state;
+    }
+    last_toggle_state = curr_toggle_state;
+    if(!state) { //default pot reading
+      frequency = get_freq_pot();
+    }else { //channel seeker
+      get_button_read();
+    }
+    radio.setFrequency(frequency);
+    //radioInfo.setBandFrequency(RADIO_BAND_FM, int(frequency*10));
+    if(!state) {
+      Serial.print("Pot: ");
+    }else {
+      Serial.print("Button: ");
+    }
+    Serial.print(frequency);
+    Serial.print(", Unfiltered: ");
+    Serial.println(analogRead(FREQ));
     draw_main();
-  } //Check if the rds is there or not if not draw the default one with no info
-  else {
-    draw_real();
   }
-  */
-  delay(500);
 }
